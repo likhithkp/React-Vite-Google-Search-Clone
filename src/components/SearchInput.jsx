47b10a5015384de723c18f6b8ce2ef572324751a
Likhith K.P <likhithkpnet@gmail.com>
2023-11-15 20:32:04 +0530

    Taiwind update

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -18,7 +18,7 @@ const SearchInput = () => {
     };
 
     return(
-        <div id="searchbox" className="h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0">
+        <div id="searchbox" className="h-[46px] w-full md:w-[580px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0">
             <AiOutlineSearch size={18} color="#9aa0a6"/>
             <input type="text" onChange={(e) => setSearchQuery(e.target.value)} onKeyUp={searchQueryHandler} value={searchQuery} autoFocus className="grow outline-0 text-black/[0.87]"/>
             <div className="flex items-center gap-3">
