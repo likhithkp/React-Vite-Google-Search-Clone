0c2573accd0fb7bbe717d67f313dea8810e5b86e
Likhith K.P <likhithkpnet@gmail.com>
2023-11-17 06:58:11 +0530

    env variables

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1"
 
 const params = {
-    key : "AIzaSyA0Por6g4kxUaHHLp-dvgE0BhZ-OiyvZE0",
-    cx : "524dffce921304f09",
+    key : import.meta.env.VITE_GOOGLE_API_KEY,
+    cx : import.meta.env.VITE_GOOGLE_API_CX,
 }
 
 export const fetchDataFromApi = async (payload) => {
